5e1cc6e4ab87d6e90a409f7d51dbdc73a343f3d0
Dmitry Salakhov <to@dimasalakhov.com>
2022-10-03 13:24:59 +1300

    feat: move jwt lib to v4

--- api/jwt/jwt_test.go
+++ api/jwt/jwt_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/golang-jwt/jwt"
+	"github.com/golang-jwt/jwt/v4"
 	portainer "github.com/portainer/portainer/api"
 	"github.com/stretchr/testify/assert"
 )
