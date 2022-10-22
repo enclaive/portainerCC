3a32df996f8b9386daa221615f8384d9d1f02963
marcely0 <marcelt91@live.de>
2022-10-22 17:04:47 +0200

    init

---
+++ api/http/handler/portainercc/key.go
@@ -0,0 +1,27 @@
+package portainercc
+
+import (
+	"encoding/json"
+	"net/http"
+
+	httperror "github.com/portainer/libhttp/error"
+	portainer "github.com/portainer/portainer/api"
+)
+
+type KeyParams struct {
+	KeyType            string
+	Description        string
+	TeamAccessPolicies portainer.TeamAccessPolicies
+	Data               string
+}
+
+func (handler *Handler) generateOrImport(w http.ResponseWriter, r *http.Request) *httperror.HandlerError {
+	var params KeyParams
+	err := json.NewDecoder(r.Body).Decode(&params)
+
+	if err != nil {
+		return httperror.BadRequest("request body malefomred", err)
+	}
+
+	return httperror.BadRequest("invalid body content", nil)
+}
